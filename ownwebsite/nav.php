9f76ae478645735f1aba2d7ea254b8474bdc3eef
joel997 <38638149+joel997@users.noreply.github.com>
2018-05-23 13:13:03 +1000

    Add files via upload

---
+++ ownwebsite/nav.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Navigation Bar Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    
+    <div class="navigation">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">PUBLIC EVENTS</a></li>
+        <li><a href="#">MOST VIEWED</a></li>
+        <li><a href="#">CREATE EVENTS</a></li>
+      </ul>
+    </div>
+    
+    <div class="Filter">
+      <h3>Filter Search</h3>
+      <div class="cateCoun">
+         <div class="Categories">
+           <!--Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+           Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+           Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
+           vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+           Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. 
+           Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.-->
+        </div>        
+        <div class="Countries">
+          <!--Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+          Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
+          vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+          Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. 
+          Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.-->
+        </div>
+        
+      </div>
+    </div>
+  
+  </body>
+</html>
